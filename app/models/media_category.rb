f231c3a7492d8e69577764db3c79b4833c8d04f5
Matthew Hopwood <m.hopwood@outwood.com>
2020-07-01 16:43:48 +0100

    add validations

--- app/models/media_category.rb
+++ app/models/media_category.rb
@@ -3,4 +3,5 @@ class MediaCategory < ApplicationRecord
   has_many :media_files, through: :media_file_categories
 
   validates_presence_of :name
+  validates_uniqueness_of :name
 end
--- app/models/media_type.rb
+++ app/models/media_type.rb
@@ -2,4 +2,5 @@ class MediaType < ApplicationRecord
   has_many :media_files, dependent: :restrict_with_exception
 
   validates_presence_of :name
+  validates_uniqueness_of :name
 end
