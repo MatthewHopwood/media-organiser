255701facaea4e94547fd596c2b959df1a64fed7
Matthew Hopwood <m.hopwood@outwood.com>
2020-07-01 11:13:14 +0100

    create routes for media files

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "media_files#index"
+  resources :media_files, except: :index
 end
