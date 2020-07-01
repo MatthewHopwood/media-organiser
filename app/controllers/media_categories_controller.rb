9af453d5d86248c10de6aaf1faef0771a1607bd9
Matthew Hopwood <m.hopwood@outwood.com>
2020-07-01 13:12:18 +0100

    add controller

---
+++ app/controllers/media_categories_controller.rb
@@ -0,0 +1,55 @@
+class MediaCategoriesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @media_categories = MediaCategory.order(:name)
+  end
+
+  def show
+    @media_category = find_media_category
+  end
+
+  def new
+    @media_category = MediaCategory.new
+  end
+
+  def create
+    @media_category = MediaCategory.new(media_category_params)
+    if @media_category.save
+      redirect_to media_category_path(@media_category), success: "Media Category was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @media_category = find_media_category
+  end
+
+  def update
+    @media_category = find_media_category
+    if @media_category.update(media_category_params)
+      redirect_to media_category_path(@media_category), success: "Media Category was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @media_category = find_media_category
+
+    @media_category.destroy!
+    redirect_to mmedia_categories_path, success: "Media Category was successfully deleted."
+
+  end
+
+  private
+
+  def find_media_category
+    MediaCategory.find(params[:id])
+  end
+
+  def media_category_params
+    params.require(:media_category).permit(:name)
+  end
+end
