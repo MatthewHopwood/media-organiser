f01f1db9a2b0c9c551dbe71a68f444a540bb8807
Matthew Hopwood <m.hopwood@outwood.com>
2020-07-01 13:12:31 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "playlists#index"
+  resources :media_categories
   resources :media_files
   resources :media_types
   resources :playlists, except: :index
