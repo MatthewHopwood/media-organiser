5da70eb6fa728fb4911f7c2ddbf656d47788de5c
Matthew Hopwood <m.hopwood@outwood.com>
2020-07-02 09:41:45 +0100

    add image model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_151425) do
+ActiveRecord::Schema.define(version: 2020_07_02_083644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "images", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "file_path", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "media_categories", force: :cascade do |t|
     t.string "name", null: false
---
+++ spec/factories/images.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :image do
+    name { "MyString" }
+    file_path { "MyString" }
+  end
+end
